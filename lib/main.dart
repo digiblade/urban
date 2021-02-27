f251864a33976f216ebcfb7f3ebe83eea0510992
digiblade <ak.chourasia.007@gmail.com>
2021-02-28 00:09:09 +0530

    register ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:urban/view/Auth/Register.dart';
 import 'component/Button.dart';
 import 'component/InputField.dart';
 
@@ -35,78 +36,100 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 80,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 80,
+                    ),
+                    child: Center(
+                      child: CircleAvatar(
+                        backgroundImage:
+                            Image.asset("assets/logo/logo.jpg").image,
+                        radius: 54,
                       ),
-                      child: Center(
-                        child: CircleAvatar(
-                          backgroundImage:
-                              Image.asset("assets/logo/logo.jpg").image,
-                          radius: 54,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 64,
+                      left: 16,
+                      right: 16,
+                    ),
+                    child: InputField(
+                      controller: ctrl,
+                      borderRadius: 4,
+                      textColor: Colors.white,
+                      bgColor: Colors.white.withOpacity(0.5),
+                      hintText: "Email",
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, left: 16, right: 16),
+                    child: InputField(
+                      controller: ctrl,
+                      borderRadius: 4,
+                      textColor: Colors.white,
+                      bgColor: Colors.white.withOpacity(0.5),
+                      isPassword: true,
+                      hintText: "Password",
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          print(ctrl.text);
+                        },
+                        child: Text(
+                          "Forget Password?",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 64,
-                        left: 16,
-                        right: 16,
-                      ),
-                      child: InputField(
-                        controller: ctrl,
-                        borderRadius: 4,
-                        textColor: Colors.white,
-                        bgColor: Colors.white.withOpacity(0.5),
-                        hintText: "Email",
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Button1(
+                      onPressed: () {},
+                      text: "Register".toUpperCase(),
+                      height: 54,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8, left: 16, right: 16),
-                      child: InputField(
-                        controller: ctrl,
-                        borderRadius: 4,
-                        textColor: Colors.white,
-                        bgColor: Colors.white.withOpacity(0.5),
-                        isPassword: true,
-                        hintText: "Password",
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                child: MixedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => RegisterPage(),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Button1(
-                        onPressed: () {},
-                        text: "Register".toUpperCase(),
-                        height: 54,
-                      ),
-                    ),
-                  ],
+                    );
+                  },
+                  text: "Don't have account?",
+                  textColor: Colors.white.withOpacity(0.7),
+                  clickText: "SignUp here",
+                  clickColor: Colors.blue,
                 ),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: MixedButton(
-                    onPressed: () {},
-                    text: "Already have account?",
-                    textColor: Colors.white.withOpacity(0.7),
-                    clickText: "SignIn here",
-                    clickColor: Colors.blue,
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
--- lib/view/Auth/Register.dart
+++ lib/view/Auth/Register.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:urban/component/Button.dart';
 import 'package:urban/component/InputField.dart';
 
+import '../../main.dart';
+
 class RegisterPage extends StatelessWidget {
   final TextEditingController ctrl = TextEditingController();
   RegisterPage({Key key}) : super(key: key);
@@ -11,127 +13,116 @@ class RegisterPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 80,
-                      ),
-                      child: Center(
-                        child: CircleAvatar(
-                          backgroundImage:
-                              Image.asset("assets/logo/logo.jpg").image,
-                          radius: 54,
-                        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 80,
+                    ),
+                    child: Center(
+                      child: CircleAvatar(
+                        backgroundImage:
+                            Image.asset("assets/logo/logo.jpg").image,
+                        radius: 54,
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 64,
-                        left: 16,
-                        right: 16,
-                      ),
-                      child: InputField(
-                        controller: ctrl,
-                        borderRadius: 4,
-                        textColor: Colors.white,
-                        bgColor: Colors.white.withOpacity(0.5),
-                        hintText: "Username",
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 64,
+                      left: 16,
+                      right: 16,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8, left: 16, right: 16),
-                      child: InputField(
-                        controller: ctrl,
-                        borderRadius: 4,
-                        textColor: Colors.white,
-                        bgColor: Colors.white.withOpacity(0.5),
-                        hintText: "Email",
-                      ),
+                    child: InputField(
+                      controller: ctrl,
+                      borderRadius: 4,
+                      textColor: Colors.white,
+                      bgColor: Colors.white.withOpacity(0.5),
+                      hintText: "Username",
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8, left: 16, right: 16),
-                      child: InputField(
-                        controller: ctrl,
-                        borderRadius: 4,
-                        textColor: Colors.white,
-                        bgColor: Colors.white.withOpacity(0.5),
-                        hintText: "Mobile",
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, left: 16, right: 16),
+                    child: InputField(
+                      controller: ctrl,
+                      borderRadius: 4,
+                      textColor: Colors.white,
+                      bgColor: Colors.white.withOpacity(0.5),
+                      hintText: "Email",
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8, left: 16, right: 16),
-                      child: InputField(
-                        controller: ctrl,
-                        borderRadius: 4,
-                        textColor: Colors.white,
-                        bgColor: Colors.white.withOpacity(0.5),
-                        isPassword: true,
-                        hintText: "Password",
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, left: 16, right: 16),
+                    child: InputField(
+                      controller: ctrl,
+                      borderRadius: 4,
+                      textColor: Colors.white,
+                      bgColor: Colors.white.withOpacity(0.5),
+                      hintText: "Mobile",
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8, left: 16, right: 16),
-                      child: InputField(
-                        controller: ctrl,
-                        borderRadius: 4,
-                        textColor: Colors.white,
-                        bgColor: Colors.white.withOpacity(0.5),
-                        isPassword: true,
-                        hintText: "Password",
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, left: 16, right: 16),
+                    child: InputField(
+                      controller: ctrl,
+                      borderRadius: 4,
+                      textColor: Colors.white,
+                      bgColor: Colors.white.withOpacity(0.5),
+                      isPassword: true,
+                      hintText: "Password",
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {
-                            print(ctrl.text);
-                          },
-                          child: Text(
-                            "Forget Password?",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, left: 16, right: 16),
+                    child: InputField(
+                      controller: ctrl,
+                      borderRadius: 4,
+                      textColor: Colors.white,
+                      bgColor: Colors.white.withOpacity(0.5),
+                      isPassword: true,
+                      hintText: "Confirm Password",
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Button1(
-                        onPressed: () {},
-                        text: "Login".toUpperCase(),
-                        height: 54,
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 24,
                     ),
-                  ],
+                    child: Button1(
+                      onPressed: () {},
+                      text: "Register".toUpperCase(),
+                      height: 54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                child: MixedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => MyApp(),
+                      ),
+                    );
+                  },
+                  text: "Already have account?",
+                  textColor: Colors.white.withOpacity(0.7),
+                  clickText: "SignIn here",
+                  clickColor: Colors.blue,
                 ),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: MixedButton(
-                    onPressed: () {},
-                    text: "Don't have account?",
-                    textColor: Colors.white.withOpacity(0.7),
-                    clickText: "SignUp here",
-                    clickColor: Colors.blue,
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
