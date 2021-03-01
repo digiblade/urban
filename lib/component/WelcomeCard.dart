c17af4fb4d8c8b9609df2cdb5de56abb91ba7776
digiblade <ak.chourasia.007@gmail.com>
2021-03-02 00:19:30 +0530

    Home page welcome card

---
+++ lib/component/WelcomeCard.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class WelcomeCard extends StatelessWidget {
+  final String mentioned;
+  final String welcomeMsg;
+  final String msg;
+  final String greetings;
+  const WelcomeCard({
+    Key key,
+    this.mentioned,
+    this.welcomeMsg,
+    this.msg,
+    this.greetings,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shadowColor: Color(0xff040077),
+      child: Container(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (mentioned != null)
+                Text(
+                  mentioned,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              if (welcomeMsg != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(welcomeMsg),
+                ),
+              if (msg != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(msg),
+                ),
+              if (greetings != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(greetings),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
